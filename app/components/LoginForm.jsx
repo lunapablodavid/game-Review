4ff346c48bcd290a5388632fdd82c52c8db02402
Matias A. Eguia <matias_eguia@hotmail.com>
2024-05-13 17:49:54 -0300

    Iniciar Sesion

---
+++ app/components/LoginForm.jsx
@@ -0,0 +1,52 @@
+"use client"
+
+import React, { useState } from 'react';
+import { AuthContext } from '../context/authContext';
+import { useContext } from "react";
+
+const LoginForm = () => {
+
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  //const {isLoggin} = useContext(AuthContext)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const res = await fetch("http://localhost:3000/auth/login", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password })
+    })
+    if (!res.ok) return alert('Usuario y/o contraseña incorrecto.')
+
+    const data = await res.json()
+
+    //Guardamos el token para su posterior uso en otras request!
+    localStorage.setItem('token', data.access_token)
+    window.location.href = '/';
+  }
+
+  return (
+    <div className="mt-20 w-1/3 p-4 bg-black bg-opacity-30 rounded-lg shadow-md">
+      <h2 className="text-2xl text-gray-500 font-semibold mb-4">Iniciar sesión</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-500 font-semibold mb-2">Usuario</label>
+          <input type="text" onChange={(e) => setUserName(e.target.value)} id="username" name="username" className="border text-black border-gray-300 px-3 py-2 w-full rounded-md focus:outline-none focus:border-blue-500" />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-gray-500 font-semibold mb-2">Contraseña</label>
+          <input type="password" onChange={(e) => setPassword(e.target.value)} id="password" name="password" className="border text-black border-gray-300 px-3 py-2 w-full rounded-md focus:outline-none focus:border-blue-500" />
+        </div>
+        <div className="flex justify-between">
+          <button type="submit" className="bg-gray-600 text-white px-4 py-2 rounded-md hover:bg-gray-500 focus:outline-none focus:bg-blue-600">Iniciar sesión</button>
+          <button type="button" className="bg-gray-700 text-gray-200 px-4 py-2 rounded-md hover:bg-gray-400 focus:outline-none focus:bg-gray-400">Salir</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
--- app/components/Presentacion.jsx
+++ app/components/Presentacion.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
+import Link from 'next/link';
 
 const Presentacion = () => {
     return (
@@ -38,9 +39,11 @@ const Presentacion = () => {
                         facere beatae accusantium aliquid eos?
                     </p>
                     <div>
+                        
                         <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-orange-300 border hover:border-pink-700 text-white'>
-                            Iniciar sesión
+                        <Link href={'/login'}> Iniciar sesión</Link>
                         </button>
+                        
                         <button className='px-6 py-3 w-full sm:w-fit bg-trasnparent rounded-full hover:bg-slate-800 text-white border border-white mt-3'>
                             Registrate
                         </button>
