9fce199a98b582374448a07bcb6401cd104a443b
Matias A. Eguia <matias_eguia@hotmail.com>
2024-05-16 20:46:32 -0300

    Correccion de error cuando la cookie no existia

--- app/components/Presentacion.jsx
+++ app/components/Presentacion.jsx
@@ -11,11 +11,9 @@ const Presentacion = () => {
 
     useEffect(() => {
         const userData = JSON.parse(localStorage.getItem('data'));
-        if (userData.isLogged===true) {
-            setData(userData);
-        }
+        if (userData) setData(userData)
     }, []);
-    
+
     return (
         <section >
             <div className='grid grid-cols-1 sm:grid-cols-12'>
@@ -45,23 +43,23 @@ const Presentacion = () => {
                         />
                     </span>
                     <p className='text-[#adb7be] text-base sm:text-lg mb-6 lg:text-xl'>
-                    ¡Bienvenidos a PlayerReview! Aquí encontrarás las mejores reseñas de videojuegos,
-                     hechas por y para gamers. Explora análisis detallados, descubre nuevos títulos y
-                      comparte tus opiniones. ¡Únete a nuestra comunidad y lleva tu experiencia de juego
-                       al siguiente nivel!
+                        ¡Bienvenidos a PlayerReview! Aquí encontrarás las mejores reseñas de videojuegos,
+                        hechas por y para gamers. Explora análisis detallados, descubre nuevos títulos y
+                        comparte tus opiniones. ¡Únete a nuestra comunidad y lleva tu experiencia de juego
+                        al siguiente nivel!
                     </p>
                     {!sessionData.isLogged &&
-                    <div>
-                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-orange-300 border hover:border-pink-700 text-white'>
-                            <Link href={'/login'}> Iniciar sesión</Link>
-                        </button>
+                        <div>
+                            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-orange-300 border hover:border-pink-700 text-white'>
+                                <Link href={'/login'}> Iniciar sesión</Link>
+                            </button>
 
-                        <button className='px-6 py-3 w-full sm:w-fit bg-trasnparent rounded-full hover:bg-slate-800 text-white border border-white mt-3'>
-                            Registrate
-                        </button>
-                    </div>||<div className='col-span-5 place-self-center mt-4 lg:mt-0'><h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+                            <button className='px-6 py-3 w-full sm:w-fit bg-trasnparent rounded-full hover:bg-slate-800 text-white border border-white mt-3'>
+                                Registrate
+                            </button>
+                        </div> || <div className='col-span-5 place-self-center mt-4 lg:mt-0'><h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
                             <span className='text-transparent bg-clip-text bg-gradient-to-br from-yellow-500 via-red-500 to-green-300'>
-                               Hola {sessionData.username}, que bueno es verte por aqui!
+                                Hola {sessionData.username}, que bueno es verte por aqui!
                             </span>
                         </h1></div>
                     }
