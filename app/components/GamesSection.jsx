959edc480bd2dd1fce47197580022e40a1d35eec
Pablo Luna <lunapablodavid@.com>
2024-07-15 21:23:15 -0300

    prueba

--- app/components/GamesSection.jsx
+++ app/components/GamesSection.jsx
@@ -13,7 +13,7 @@ const GamesSection = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                let url = 'http://localhost:3000/video_games/';
+                let url = 'http://localhost:3000/video_game/';
                 if (tag !== 'Todos') {
                     url += `category/${tag}`;
                 }
