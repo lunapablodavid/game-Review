341de8a4d176c6369834f63f24211aa0db00a3d9
Pablo Luna <lunapablodavid@.com>
2024-07-16 21:51:06 -0300

    consola

--- app/components/Consolas.jsx
+++ app/components/Consolas.jsx
@@ -1,95 +1,79 @@
-// Consolas.jsx
-"use client"
-import React, { useContext, useTransition, useState } from 'react';
+"use client";
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import TabButton from './TabButton';
 import { useUser } from '../context/UserContext'; // Ajusta la ruta según tu estructura de archivos
 
-const tab_data = [{
-    title: 'Portatiles',
-    id: 'portatiles',
-    content: (
-        <ul>
-            <li>GameBoy Advance</li>
-            <li>PSP</li>
-            <li>PS-VITA</li>
-            <li>Nintendo Switch</li>
-        </ul>
-    )
-},
-{
-    title: 'Sobremesa',
-    id: 'Sobremesa',
-    content: (
-        <ul>
-            <li>PS 3</li>
-            <li>PS 4</li>
-            <li>PS 5</li>
-            <li>Xbox 360</li>
-            <li>Xbox One</li>
-            <li>Xbox Series s/x</li>
-        </ul>)
-},
-{
-    title: 'PC',
-    id: 'PC',
-    content: (
-        <ul>
-            <li>Procesadores</li>
-            <li>Placas de video</li>
-        </ul>)
-}
-];
-
 export const Consolas = () => {
     const [tab, setTab] = useState("portatiles");
-    const [isPending, startTransition] = useTransition();
+    const [consolas, setConsolas] = useState([]);
     const { userData } = useUser(); // Cambiado de SessionContext a UserContext
 
+    useEffect(() => {
+        const fetchConsolas = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/console');
+                if (!response.ok) {
+                    throw new Error('Error al obtener las consolas');
+                }
+                const data = await response.json();
+                setConsolas(data);
+            } catch (error) {
+                console.error('Error al obtener las consolas:', error);
+            }
+        };
+
+        fetchConsolas();
+    }, []);
+
     const handleTabChange = (id) => {
-        startTransition(() => {
-            setTab(id);
-        })
-    }
+        setTab(id);
+    };
+
+    const renderTabContent = () => {
+        const selectedTab = consolas.find((c) => c.id === tab);
+        if (!selectedTab) {
+            return null; // Puedes manejar aquí cómo mostrar si no hay datos para la pestaña seleccionada
+        }
+
+        return (
+            <ul>
+                {selectedTab.consoles.map((console) => (
+                    <li key={console.id}>{console.name}</li>
+                ))}
+            </ul>
+        );
+    };
 
     return (
         <section className='text-white'>
-            {userData.name &&
-            
-            <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-                <Image src='/images/consolas.jpg' alt='imagen consolas' width={500} height={500} />
-                <div>
-                    <h2 className='text-4xl text-white font-bold mb-4'>Consolas:</h2>
-                    <p className='text-base lg:text-lg'>¡Bienvenidos a nuestra plataforma, el destino definitivo para los amantes de los videojuegos!
-                         En nuestro sitio, hemos reunido una impresionante colección de consolas de videojuegos para satisfacer a todos los jugadores,
-                          desde los aficionados hasta los más dedicados. Aquí te presentamos un recorrido por todas las consolas de videojuegos que puedes
-                           encontrar en nuestro sitio.</p>
-                    <div className='flex flex-row mt-8'>
-                        <TabButton
-                            selectTab={() => handleTabChange("portatiles")}
-                            active={tab === "portatiles"}>
-                            {""}
-                            portatiles{""}
-                        </TabButton>
-                        <TabButton
-                            selectTab={() => handleTabChange("Sobremesa")}
-                            active={tab === "Sobremesa"}>
-                            {""}
-                            Sobremesa{""}
-                        </TabButton>
-                        <TabButton
-                            selectTab={() => handleTabChange("PC")}
-                            active={tab === "PC"}>
-                            {""}
-                            PC{""}
-                        </TabButton>
+            {userData.name && (
+                <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+                    <Image src='/images/consolas.jpg' alt='imagen consolas' width={500} height={500} />
+                    <div>
+                        <h2 className='text-4xl text-white font-bold mb-4'>Consolas:</h2>
+                        <p className='text-base lg:text-lg'>
+                            ¡Bienvenidos a nuestra plataforma, el destino definitivo para los amantes de los videojuegos!
+                            En nuestro sitio, hemos reunido una impresionante colección de consolas de videojuegos para satisfacer a todos los jugadores,
+                            desde los aficionados hasta los más dedicados. Aquí te presentamos un recorrido por todas las consolas de videojuegos que puedes encontrar en nuestro sitio.
+                        </p>
+                        <div className='flex flex-row mt-8'>
+                            <TabButton selectTab={() => handleTabChange("portatiles")} active={tab === "portatiles"}>
+                                Portátiles
+                            </TabButton>
+                            <TabButton selectTab={() => handleTabChange("Sobremesa")} active={tab === "Sobremesa"}>
+                                Sobremesa
+                            </TabButton>
+                            <TabButton selectTab={() => handleTabChange("PC")} active={tab === "PC"}>
+                                PC
+                            </TabButton>
+                        </div>
+                        <div className='mt-8'>{renderTabContent()}</div>
                     </div>
-                    <div className='mt-8'> {tab_data.find((t) => t.id === tab).content}</div>
                 </div>
-            </div>
-            }
+            )}
         </section>
-    )
-}
+    );
+};
 
 export default Consolas;
